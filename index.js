07cc926833c39ee9d8a48794f7fbc147f0267f80
Obasi Chizitere <blossomobasi2@gmail.com>
2024-05-14 15:56:20 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDb...", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function createCourse() {
+  const course = new Course({
+    name: "Angular Course",
+    author: "Blossom",
+    tags: ["angular", "frontend"],
+    isPublished: true,
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+async function getCourse() {
+  const courses = await Course.find({
+    author: "Blossom",
+    isPublished: true,
+  })
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 });
+  console.log(courses);
+}
+
+getCourse();
